2e37b73b9a542b8ea5ec3337cbce2189d2f0d6fa
LeticiaSiqueirat4857 <leticia.siqueirat4857@gmail.com>
2024-10-18 11:26:48 -0300

    Add files via upload

---
+++ 18-10/script.js
@@ -0,0 +1,20 @@
+//crio uma variável e dentro dela eu coloco o elemento html que tem o ID "botao"
+const botao= document.getElementById("botao")
+
+//uma função que aumenta o tamanho da fonte
+function aumentaFonte () {
+paragrafo.style.fontSize ="30px"
+}
+
+//para chamar a função quando eu quiser , eu adiciono um VENTO, que vai ocorrer eu clicar no botao
+botao.addEventListener("click" , aumentaFonte)
+
+//crie um novo botao
+//crie um novo parágrafo
+//no javascript, crie a variável que vai receber esse botao (não esqueça do ID)
+//depois,embaixo,crie uma função que troca a cor e a o tipo da fonte do parágrafo
+//depois,mais embaixo,chame essa função quando clicar neste segundo botao (use addEventListener)
+
+const Botão = document.getElementById("Botão")
+const meuParágrafo = document.getElementById("Parágrafo")
+
